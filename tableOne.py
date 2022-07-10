78a4f30e3555c044d1ce0504df2a7e889e1ebe71
Xinyu Zhang <xz2691@columbia.edu>
2022-07-10 17:09:09 -0400

    format fixed.

--- tableOne.py
+++ tableOne.py
@@ -27,13 +27,13 @@ def parallelRun(poolParam):
 
 
 if __name__ == '__main__':
-    nExperimentReptition = 500
+    nExperimentReptition = 200
     randomSeed = 20220222
     dataDistributions = ['gamma', 'lognorm']
     thresholdPercentages = np.linspace(0.6, 0.85, 11)
     # served as the lhsEndpoint in the objective function: 1_{lhs<=x<=rhs}.
     percentageLHSs = np.linspace(0.9, 0.99, 10)
-    dataSizes = [500, 800, 1000, 1200, 1500, 1800, 2000, 2500, 3000]
+    dataSizes = [500, 800]
     for dataDistribution, dataSize, percentageLHS, thresholdPercentage in itertools.product(*[dataDistributions, dataSizes, percentageLHSs, thresholdPercentages]):
         metaDataDict["dataSize"] = dataSize
         metaDataDict["percentageLHS"] = percentageLHS
@@ -48,16 +48,21 @@ if __name__ == '__main__':
         FILE_NAME += ["randomSeed="+str(randomSeed)]
         FILE_NAME += ["nExperimentReptition="+str(nExperimentReptition)]
         FILE_NAME = '_'.join(FILE_NAME)+".csv"
-        if os.path.exists(os.path.join(FILE_DIR, FILE_NAME)+".csv"):
+        if os.path.exists(os.path.join(FILE_DIR, FILE_NAME)):
             print("Note: Already exists! Write: " +
                   os.path.join(FILE_DIR, FILE_NAME))
         else:
             with Pool() as p:
-                pd.DataFrame(np.asarray(p.map(parallelRun, poolParamList))
-                             ).to_csv(os.path.join(FILE_DIR, FILE_NAME),
+                df = pd.DataFrame(np.asarray(p.map(parallelRun, poolParamList))
+                             )
+                print(df.mean(axis=0).values)
+                df.to_csv(os.path.join(FILE_DIR, FILE_NAME),
                                       header=["(0,KS)", "(1,KS)", "(2,KS)",
                                               "(0,CHI2)", "(1,CHI2)", "(2,CHI2)"],
                                       index=True,
                                       index_label="Experiment Repetition Index")
+                del df
+                
             print("Write: " +
                   os.path.join(FILE_DIR, FILE_NAME))
+

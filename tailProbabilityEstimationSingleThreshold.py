ab37fdc0c79943939fb83b57116addde02e869e4
Xinyu Zhang <xz2691@columbia.edu>
2022-07-20 11:06:17 -0400

    catch and print out error messages. Fix the OS error due to too long the file name.

--- tailProbabilityEstimationSingleThreshold.py
+++ tailProbabilityEstimationSingleThreshold.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 import os
 import itertools
+import sys
+import traceback
+
 FILE_DIR = "testResultSmall"
 metaDataDict = {"dataSize": 500,
                 "percentageLHS": 0.99,
@@ -33,7 +36,7 @@ if __name__ == '__main__':
     nExperimentReptition = 10
     randomSeed = 20220222
     trueValue = 0.005
-    dataDistributions = ['gamma', 'lognorm']
+    dataDistributions = ['gamma', 'lognorm', 'pareto']
     thresholdPercentages = np.linspace(0.6, 0.85, 11).tolist()
     # served as the lhsEndpoint in the objective function: 1_{lhs<=x<=rhs}.
     percentageLHSs = np.linspace(0.9, 0.99, 10).tolist()
@@ -53,6 +56,7 @@ if __name__ == '__main__':
         FILE_NAME += ["randomSeed="+str(randomSeed)]
         FILE_NAME += ["nExperimentReptition="+str(nExperimentReptition)]
         FILE_NAME = '_'.join(FILE_NAME)+".csv"
+        FILE_NAME = FILE_NAME.replace("00000000000001","").replace("0000000000001","")
         if os.path.exists(os.path.join(FILE_DIR, FILE_NAME)):
             print("Note: Already exists! Write: " +
                   os.path.join(FILE_DIR, FILE_NAME))
@@ -74,5 +78,23 @@ if __name__ == '__main__':
                               index_label="Experiment Repetition Index")
                     del df
                 print("Success!")
-            except:
+            except BaseException as ex:
                 print("Fail on "+os.path.join(FILE_DIR, FILE_NAME))
+                os.remove(os.path.join(FILE_DIR, FILE_NAME))
+
+                # Get current system exception
+                ex_type, ex_value, ex_traceback = sys.exc_info()
+
+                # Extract unformatter stack traces as tuples
+                trace_back = traceback.extract_tb(ex_traceback)
+
+                # Format stacktrace
+                stack_trace = list()
+
+                for trace in trace_back:
+                    stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" % (
+                        trace[0], trace[1], trace[2], trace[3]))
+
+                print("Exception type : %s " % ex_type.__name__)
+                print("Exception message : %s" % ex_value)
+                print("Stack trace : %s" % stack_trace)
--- testRunOnAllTasks.sh
+++ testRunOnAllTasks.sh
@@ -4,7 +4,7 @@ echo "Start to run all four tasks."
 echo "Run on tail probability estimation problem. "
 
 echo "Warming up. Run a small example."
-python tailProbabilityEstimationUnit.py
+#python tailProbabilityEstimationUnit.py
 
 echo "Run on tail probability estimation--single threshold."
 python tailProbabilityEstimationSingleThreshold.py
@@ -14,11 +14,11 @@ python tailProbabilityEstimationMultipleThresholds.py
 
 echo "Run on tail quantile estimation problem. "
 echo "Warming up. Run a small example."
-python quantileEstimationUnit.py
+#python quantileEstimationUnit.py
 
 echo "Run on quantile estimation--single threshold."
 python quantileEstimationSingleThreshold.py
 
-echo "Run on quantile estimation--mmultiple thresholds."
+echo "Run on quantile estimation--multiple thresholds."
 python quantileEstimationMultipleThresholds.py
 

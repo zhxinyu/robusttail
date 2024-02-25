aa41c02b303f5208bf9ce9e676501a83ed107adc
Xinyu Zhang <52359054+zhxinyu@users.noreply.github.com>
2024-02-25 15:36:01 -0500

    Bayesian estimation for tail probability internval.

--- table5_bayesian.py
+++ table5_bayesian.py
@@ -14,11 +14,15 @@ utils = importr('utils')
 try:
 	importr('POT')
 	importr('nloptr')    
+	importr('MASS')    
 except:
 	utils.install_packages('POT', contribulr="https://cran.microsoft.com/")
 	utils.install_packages('nloptr', contribulr="https://cran.microsoft.com/")
+	utils.install_packages('MASS', contribulr="https://cran.microsoft.com/")
 	importr('POT')
 	importr('nloptr')    
+	importr('MASS')    
+ 
 
 POTUtilityinR='''
 gof_find_threshold <- function(data, bootstrap=FALSE) {
@@ -86,21 +90,7 @@ pgpd <- function(x, loc, scale, shape) {
     return(1-(1+shape/scale*(x-loc))**(-1/shape))
   }
 }
-dgpd <-function(x, loc, scale, shape) {
-  if (shape == 0) {
-    return(1/scale*exp(-(x-loc)/scale))
-  } else {
-    return(1/scale*(1+shape/scale*(x-loc))**(-1/shape-1))
-  }  
-}
-d_pgpd_d_scale <- function(x, loc, scale, shape) {
-  return(-dgpd(x, loc, scale, shape)*(x-loc)/scale)
-}
-d_pgpd_d_shape <- function(x, loc, scale, shape) {
-  first_part <- -(1-pgpd(x, loc, scale, shape))/shape**2*log(1+shape*(x-loc)/scale)
-  second_part <- dgpd(x, loc, scale, shape)*(x-loc)/shape
-  return(first_part+second_part)
-}
+
 eval_llh_f <- function(x, data, u) {
   objective <- gpd_neg_loglikelihood(x, data, u)
   grad <- gpd_neg_loglikelihood_derivative(x, data, u)
@@ -117,29 +107,52 @@ eval_llh_g_ineq <- function(x, data, u) {
     grad   <- cbind(grad1_scale, grad1_shape)
     return( list( "constraints"=constr, "jacobian"=grad ) )
 }
-eval_tip_f <- function(x, data, u, direction, lhs, rhs, llmax, cutoff) {
+
+eval_tip <- function(x, data, u, lhs, rhs) {
   scale <- x[1]
   shape <- x[2]
   tail_internval_probability <- pgpd(rhs, u, scale, shape) - pgpd(lhs, u, scale, shape)
-  objective <- direction*tail_internval_probability
-  dout_dscale <- direction*(d_pgpd_d_scale(rhs, u, scale, shape) - d_pgpd_d_scale(lhs, u, scale, shape))
-  dout_dshape <- direction*(d_pgpd_d_shape(rhs, u, scale, shape) - d_pgpd_d_shape(lhs, u, scale, shape))
-  grad <- c(dout_dscale, dout_dshape)
-  return( list( "objective"=objective, "gradient"=grad ) )
+  return(tail_internval_probability)
 }
-eval_tip_g_ineq <- function(x, data, u, direction, lhs, rhs, llmax, cutoff) {
-    xx_u = data[data>u] -u
-    scale <- x[1]
-    shape <- x[2]
-    constr1 <- (-2) *( -gpd_neg_loglikelihood(x=x, data=data, u=u) - llmax ) - cutoff
-    constr2 <- -shape*(xx_u)-scale
-    grad1   <- 2*gpd_neg_loglikelihood_derivative(x=x, data=data, u=u)
-    grad2_scale  <- -rep(1, length(xx_u))
-    grad2_shape  <- -xx_u
-    constr <- c(constr1, constr2)
-    grad   <- rbind(grad1, cbind(grad2_scale, grad2_shape))
-    return( list( "constraints"=constr, "jacobian"=grad ) )
+
+sampleProposal <- function(x0, prior_scale, prior_shape, u) {
+  # Generate samples
+  repeat {
+    xstar <- mvrnorm(1, mu = x0, Sigma = matrix(c(1, 0, 0, 1), nrow = 2))
+    if (xstar[1] > prior_scale[1] && xstar[1] < prior_scale[2] &&
+        xstar[2] > prior_shape[1] && xstar[2] < prior_shape[2]
+    ) {
+      # notice the proposal probability distributio ratio is equal to 1 so we skip the term
+      # in the code, i.e. Q(xstar|x0) = Q(x0|xstar) if Q(x'|x) ~ N(x, I). 
+      accrej <- exp( min(0, -gpd_neg_loglikelihood(xstar, data, u) + 
+                             gpd_neg_loglikelihood(x0,    data, u))
+      )
+      if (runif(1, min = 0, max = 1) < accrej) {
+        return(xstar)
+      }
+      
+    }
+  }
 }
+
+estimateTIPviaMCMC <- function(x0, prior_scale, prior_shape, 
+                               data, u, lhs, rhs, conf) {
+  burnIn <- 20000
+  for ( i in 1:burnIn ){
+    x0 <- sampleProposal(x0, prior_scale, prior_shape, u)
+  }
+  chainLength <- 1000
+  tip <- rep(NA, chainLength)
+  tip_idx <- 1 
+  base_probability <- sum(data>u)/length(data)   
+  for ( i in 1 : chainLength  ){
+    tip[tip_idx] = base_probability*eval_tip(x=x0, data=data, u=u, lhs=lhs, rhs=rhs)
+    x0 <- sampleProposal(x0, prior_scale, prior_shape, u)
+    tip_idx = tip_idx + 1    
+  }
+  return(quantile(tip, probs = c(0, conf), na.rm=TRUE))
+}
+
 gpdTIP <- function(data, lhs, rhs, conf = .95) {
 
   if(lhs >= rhs) {
@@ -151,94 +164,59 @@ gpdTIP <- function(data, lhs, rhs, conf = .95) {
     u <- gof_find_threshold(data, bootstrap=TRUE)
   }
   base_probability <- sum(data>u)/length(data) 
-  # Find initial point for the following non-linear optimization problem
+  # Find initial point for the following MCMC method to estimate credible interval for TIP.
   suppressWarnings(z <- POT::fitgpd(data, threshold = u, est = 'mle'))
   x0 = c(as.numeric(z$param[1]), as.numeric(z$param[2]))
+  estimate <- NaN
 
-  ## Find the maximum likelihood estimation and log likelihood value. 
-  ## use either NLOPT_LD_SLSQP, LD_MMA
-  local_opts1 <- list(   "algorithm"  = "NLOPT_LD_MMA",
-                        "xtol_rel"   = 0,
-                        "xtol_abs"   = 1e-6,
-                        "maxeval"    = 10000,
-                        "check_derivatives" = FALSE,
-                        "check_derivatives_print" = "errors",                        
-                        "check_derivatives_tol" = 1e-04)
-  local_res <- nloptr::nloptr(x0=x0, 
-                              eval_f=eval_llh_f, 
-                              lb=c(0, -Inf), ub=c(Inf, Inf), 
-                              eval_g_ineq=eval_llh_g_ineq,
-                              opts=local_opts1,
-                              data=data, u=u)
-  llmax <- -local_res$objective
-  x0 <- c(local_res$solution[1], local_res$solution[2])  
-  cutoff <- qchisq(p=conf, df=1)
-  estimate <- base_probability*eval_llh_f(x=x0, data=data, u=u)$objective
+  prior_scale = c(max(0, x0[1] -1), x0[1] + 1)
+  prior_shape = c(x0[2] - 1, x0[2] + 1)
 
-  ## NLOPT_LD_SLSQP, LD_MMA
-  local_opts2 <- list(   "algorithm"  = "NLOPT_LD_MMA",
-                        "xtol_rel"   = 0,
-                        "xtol_abs"   = 1e-6,
-                        "maxeval"    = 10000,
-                        "check_derivatives" = FALSE,
-                        "check_derivatives_print" = "errors",
-                        "check_derivatives_tol" = 1e-4)
-  direction <- 1
-  opt_res <- nloptr::nloptr(x0=x0,
-                        eval_f=eval_tip_f,
-                        lb=c(0, -Inf),
-                        ub=c(Inf, Inf),
-                        eval_g_ineq=eval_tip_g_ineq,
-                        opts=local_opts2,
-                        data=data, u=u, direction=direction, lhs=lhs, rhs=rhs, llmax=llmax, cutoff=cutoff)
-  local_min_val <- direction*opt_res$objective*base_probability
+  bound_val <- estimateTIPviaMCMC(x=x0, prior_scale=prior_scale, prior_shape=prior_shape,
+                                  data=data, u=u, lhs=lhs, rhs=rhs, conf=.95)
 
-  direction <- -1
-  opt_res <- nloptr::nloptr(x0=x0,
-                        eval_f=eval_tip_f,
-                        lb=c(0, -Inf),
-                        ub=c(Inf, Inf),
-                        eval_g_ineq=eval_tip_g_ineq,
-                        opts=local_opts2,
-                        data=data, u=u, direction=direction, lhs=lhs, rhs=rhs, llmax=llmax, cutoff=cutoff)
-  local_max_val <- direction*opt_res$objective*base_probability  
-   
-  CI <- c(local_min_val, local_max_val)
+  CI <- c(bound_val[1], bound_val[2])
   out <- list(estimate, CI, conf)
   names(out) <- c("Estimate", "CI", "ConfLevel")
-  return(out)
+  out
 }
 
     '''
 
 if __name__ == '__main__':
+    import argparse  
+    FILE_DIR = "large"
+    
+    parser = argparse.ArgumentParser(description='TIP estimation using MCMC.')
+    parser.add_argument('lhs_st', type=float, help='start of LHS list in the objective function')
+    parser.add_argument('ds', type=str, help='Data source for simulation')
+    args = parser.parse_args()
+    
     randomSeed = 20220222
     stringToDataModule = {"gamma": gamma,
 						"lognorm": lognorm,
 						"pareto": pareto}
     trueValue = 0.005
-    percentageLHSs = np.linspace(0.9, 0.99, 10).tolist()
-
+    # percentageLHSs = np.linspace(0.9, 0.99, 10).tolist()
+    # dataSources = ['gamma','lognorm','pareto']
+    percentageLHSs = [args.lhs_st]
+    # percentageLHSs = np.arange(args.lhs_st, args.lhs_ed, 0.01)
+    dataSources = [ args.ds ]
     metaDataDict = {"dataSize": 500}
-    dataSources = ['gamma','lognorm','pareto']
+    
     nrep = 200
     result = []
     columns = ["Data Source", 'nData',"percentageLHS", "Lower Bound","Upper Bound", "True Value", "Repetition Index"]
     for percentageLHS in percentageLHSs:
-        # if percentageLHS != 0.98:
-        #     continue
+        percentageLHS = np.round(percentageLHS,2)
         percentageRHS = percentageLHS + trueValue
         for dataSource in dataSources:
-            # if dataSource!= "pareto":
-            #     continue
             dataModule = stringToDataModule[dataSource]
             leftEndPointObjective = dpu.endPointGeneration(
                 dataModule, percentageLHS, dpu.dataModuleToDefaultParamDict[dataModule])
             rightEndPointObjective = dpu.endPointGeneration(
                 dataModule, percentageRHS, dpu.dataModuleToDefaultParamDict[dataModule])
             for nnrep in range(nrep):
-                # if nnrep!= 85:
-                #     continue
                 print(f"Working on {percentageLHS}_{dataSource}_{nnrep}")
                 try:
                     metaDataDict['random_state'] = randomSeed+nnrep
@@ -247,21 +225,24 @@ if __name__ == '__main__':
                                                     metaDataDict['dataSize'], 
                                                     metaDataDict['random_state'])
                     POTApply = '''
-                    lhs <- {:}
+lhs <- {:}
 
-                    rhs <- {:}
+rhs <- {:}
 
-                    data <- c({:})
+data <- c({:})
 
-                    out <- tryCatch(
-                    gpdTIP(data, lhs, rhs, conf=0.95), 
-                    error = function(e) e
-                    )
+out <- tryCatch(
+gpdTIP(data, lhs, rhs, conf=0.95), 
+error = function(e) e
+)
+if ("error" %in% class(out)) {{
+  print(out)
+}}
 
-                    bbd <- if ("error" %in% class(out)) NA else{{
-                        out$CI
-                    }}
-                    bbd 
+bbd <- if ("error" %in% class(out)) NA else{{
+    out$CI
+}}
+bbd 
                     '''.format(leftEndPointObjective, 
                             rightEndPointObjective, 
                             ', '.join([str(eachData)for eachData in inputData.tolist()]))
@@ -274,11 +255,9 @@ if __name__ == '__main__':
                                 percentageLHS, 0, 
                                 0, 
                                 trueValue, nnrep])
-            # assert False
             print(f"Finish experiments on {percentageLHS}-{dataSource}")
             df = pd.DataFrame(data = result, columns = columns)
-            df.to_csv(os.path.join('large', f'up_to_table5_{percentageLHS}_{dataSource}.csv'), header=columns, index = False)
+            df.to_csv(os.path.join(FILE_DIR, f'up_to_table5_{percentageLHS}_{dataSource}.csv'), header=columns, index = False)
     df = pd.DataFrame(data = result, columns = columns)
 
-    FILE_DIR = "large"
-    df.to_csv(os.path.join(FILE_DIR, 'tableFive_rev1.csv'), header=columns, index = False)
+    df.to_csv(os.path.join(FILE_DIR, f'tableFive_bayesian_{args.lhs_st}_{args.ds}.csv'), header=columns, index = False)
